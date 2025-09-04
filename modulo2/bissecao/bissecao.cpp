bb36e66ade80ca83f6111208869ccc3fc0ccfde1
Edgard <ew.engmec@gmail.com>
2025-09-04 18:54:26 -0300

    programa bissecao rodando

--- modulo2/bissecao/bissecao.cpp
+++ modulo2/bissecao/bissecao.cpp
@@ -1,21 +1,27 @@
+#include <csignal>
 #include <iostream>
 #include <math.h>
 
 using namespace std;
 
 double fdx (double a){
-    return -0.5*pow(a,2)+2.5*a+4.5;
+  return -0.5*pow(a,2)+2.5*a+4.5;
 }
 
-
 int main(){
-    double xi=5, xs=10, xm=0;
-    double tol=0;
-
+  int n=0;
+  double xi=5, xs=10, xm=0;
+  double err=0;
+  const double tol=0.0001;
+  do{
+    err=abs((xs-xi))/xi;
     if(fdx(xi)*fdx(xs) < 0){
-        xm=0.5*(xi+xs);
-    }
-
-    cout<< xm<< endl;
-    return 0;
+      xm= 0.5*(xi+xs);
+      if(fdx(xi)*fdx(xm)<0) xs=xm;
+      else if (fdx(xm)*fdx(xs)<0) xi=xm;
+    }else {cout<< "Não existe raiz neste intervalo"<< endl; break;}
+    n++;
+    cout<< n<< ": "<< xm<< " ; "<< err<<  endl;
+  }while (err >= tol);
+  return 0;
 }
